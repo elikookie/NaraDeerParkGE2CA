2c0a0b933636c1b9120ee48391c1d5f524628ea6
Onting <ontingleung@tutamail.com>
2025-05-05 14:35:08 +0100

    interact script extended

--- narage1ca/scenes/interact_ray.gd
+++ narage1ca/scenes/interact_ray.gd
@@ -11,3 +11,6 @@ func _physics_process(_delta: float) -> void:
 		
 		if collider is Interactable:
 			prompt.text = collider.prompt_message
+			
+			if Input.is_action_just_pressed("interact"):
+				collider.interact(owner)
--- narage1ca/scenes/interactable.gd
+++ narage1ca/scenes/interactable.gd
@@ -4,6 +4,9 @@ class_name Interactable
 @export var prompt_message = "Interactable"
 
 
+func interact(body):
+	print(body.name, " interacted with ", name)
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
