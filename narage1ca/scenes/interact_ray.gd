bdb1bf574210483842b5b5cabd3d0b7b2d17e936
Onting <ontingleung@tutamail.com>
2025-05-05 14:01:46 +0100

    get collider info

--- narage1ca/scenes/interact_ray.gd
+++ narage1ca/scenes/interact_ray.gd
@@ -1,7 +1,12 @@
 extends RayCast3D
 
+@onready var prompt = $Prompt
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
+	prompt.text = ""
+	
 	if is_colliding():
-		print("Decting.....")
+		var collider = get_collider()
+		
+		prompt.text = "detecting " + collider.name
